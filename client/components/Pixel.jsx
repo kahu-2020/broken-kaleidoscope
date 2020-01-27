3a6ccbc23aff3732f4d0b6193bbe5ec52a969f28
mildredthecat <57421193+mildredthecat@users.noreply.github.com>
2020-01-28 11:51:07 +1300

    changiong colors

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 // const Pixel = props => {
 //   return (
 
+  const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 class Pixel extends React.Component {
   constructor(props) {
@@ -10,21 +12,50 @@ class Pixel extends React.Component {
 
     this.state = {
       style: {
-        height: '100px',
-        width: '100px',
-        backgroundColor: 'aqua'
+        height: '50px',
+        width: '50px',
+        backgroundColor: randomHexColor()
       }
     }
+    this.randomColor=this.randomColor.bind(this)
+    this.turnBlack=this.turnBlack.bind(this)
   }
+
+  randomColor() {
+  console.log('click')
+
+
+    this.setState({
+      style:{
+        backgroundColor: randomHexColor(),
+        height: '50px',
+        width: '50px'
+
+     }
+    })
+  }
+
+  turnBlack() {
+    this.setState({
+      style:{
+        backgroundColor: 'black',
+        height: '50px',
+        width: '50px'
+  }
+})
+}
 
   render() {
     return (
-      <div style={this.state.style}>
+      <div style={this.state.style} onClick={this.randomColor} onMouseEnter={this.turnBlack}>
       </div>
     )
   }
 }
 
 
-
 export default Pixel
+
+
+//  const randomHexColor = () =>
+// `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
