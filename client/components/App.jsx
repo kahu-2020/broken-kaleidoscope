43a94670e20915d6aff8c1ff747eafe071ef0255
mildredthecat <57421193+mildredthecat@users.noreply.github.com>
2020-01-28 16:35:34 +1300

    end of exercise

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,24 @@ import Pixel from './Pixel'
 
 const App = () => {
 
-  let banana = []
+  // let t = (<Pixel />)
 
-  for (let i = 0; i < 1000; i++) {
-    banana.push(<Pixel />)
-  }
-
-
-  return banana
-
+  // return Array.from({ length: 100 }, () => t);
+  // these two functions perform the same action
+  return new Array(5000).fill(<Pixel />)
 }
+
+
+
+
+
+// let manyPixels = []
+
+// for (let i = 0; i < 1000; i++) {
+//   manyPixels.push(<Pixel />)
+// }
+
+// return manyPixels
+
+
 export default App
